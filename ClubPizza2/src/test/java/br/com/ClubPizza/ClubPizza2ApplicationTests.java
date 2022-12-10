0393811fb79785ecd5da97e8d96e9e8d0a7aa7c9
Marcos Gabriel <marcosgabriel79355@gmail.com>
2022-12-10 11:34:06 -0300

    Preparando a tela de login

---
+++ ClubPizza2/src/test/java/br/com/ClubPizza/ClubPizza2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ClubPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubPizza2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
